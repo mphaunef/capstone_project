40c5c4a487a0de583695e569e95503f7dae1824c
Megan <meganphaunef14@yahoo.com>
2022-05-03 21:39:22 -0400

    completed database models, added checklist/ideas going forward

--- model.py
+++ model.py
@@ -12,21 +12,28 @@ class User(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
 
     #relationships
+    users_to_user_songs = db.relationship('User_Song', back_populates="user_songs_to_users")
+    users_to_users_genres = db.relationship('User_Genres', back_populates="users_genres_to_users")
 
     def __repr__(self):
+        return (f"<User user_id={self.user_id} email={self.email} password={self.password} username={self.username}>")
+
 
 class User_Song(db.Model):
 
     __tablename__ = 'user_songs'
 
+    user_songs_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     song_id = db.Column(db.Integer, db.ForeignKey("songs.song_id"))
     like = db.Column(db.Boolean)
 
     #relationships
+    user_songs_to_users = db.relationship('User', back_populates="users_to_user_songs")
+    user_songs_to_songs = db.relationship('Song', back_populates="songs_to_user_songs")
 
     def __repr__(self):
-
+        return (f"<User_Song user_id={self.user_id} song_id={self.song_id} like={self.like}>")
 
 class Song(db.Model):
 
@@ -36,11 +43,15 @@ class Song(db.Model):
     name = db.Column(db.String, nullable=False)
     artist = db.Column(db.String, nullable=False)
     album = db.Column(db.String, nullable=False)
-    release_date = db.Column(db.datetime, nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+    spotify_song_id = db.Column(db.Integer, unique=True)
 
     #relationships?
+    songs_to_user_songs = db.relationship('User_Song', back_populates="user_songs_to_songs")
+    songs_to_songs_genres = db.relationship('Song_Genre', back_populates="songs_genres_to_songs")
 
     def __repr__(self):
+        return (f"<Song song_id={self.song_id} name={self.name} artist={self.artist} album={self.album} release_date={self.release_date} spotify_song_id={self.spotify_song_id}>")
 
 
 class Song_Genre(db.Model):
@@ -52,6 +63,58 @@ class Song_Genre(db.Model):
     genre_id = db.Column(db.String, db.ForeignKey('genres.genre_id'))
 
     #relationships
+    songs_genres_to_songs = db.relationship('Song', back_populates="songs_to_songs_genres")
+    songs_genres_to_genre = db.relationship('Genre', back_populates="genre_to_songs_genre")
 
     def __repr__(self):
+        return (f"<Song_Genre song_genre_id={self.song_genre_id} song_id={self.song_id} genre_id={self.genre_id}>")
+
+class Genre(db.Model):
+
+    __tablename__ = 'genres'
+
+    genre_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    genre_name = db.Column(db.String)
+
+    #relationships
+    genre_to_songs_genre = db.relationship('Song_Genre', back_populates="songs_genres_to_genre")
+    genre_to_users_genres = db.relationship('User_Genres', back_populates="users_genres_to_genre")
+    
+    def __repr__(self):
+        return (f"<Genre genre_id={self.genre_id} genre_name={self.genre_name}>")
+
+class User_Genres(db.Model):
+
+    __tablename__ = 'users_genres'
+
+    ''' All users playlists/genres '''
+
+    user_genre_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.genre_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    #relationships
+    users_genres_to_genre = db.relationship('Genre', back_populates="genre_to_users_genres")
+    users_genres_to_users = db.relationship('User', back_populates="users_to_users_genres")
+    
+    def __repr__(self):
+        return (f"<User_Genres user_genre_id={self.user_genre_id} genre_id={self.genre_id} user_id={self.user_id}>")
+
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///appdb", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
 
--- server.py
+++ server.py
@@ -2,15 +2,35 @@ from flask import (Flask, render_template, request, flash, session, redirect)
 from model import connect_to_db, db
 import jinja2
 
+app = Flask(__name__)
+app.secret_key = "dev"
+# app.jinja_env.undefined = StrictUndefined
+
 
 @app.route('/')
 def login():
     return render_template('login.html')
 
+# @app.route('/handlelogin', methods=['POST'])
+# def handle_login():
+
+#     # .get email
+#     # .get password
+#     # .get user
+
+#     # if user doesnt exist or if password is incorrect
+#     #     flash message ('Incorrect user or password. Please try again.')
+#     #     redirect to /login
+#     # else:
+#     #     session log in = True
+
+#     #     redirect to /homepage
 
 @app.route('/newuser')
 def create_new_user():
     return render_template('create_user.html')
+
+    #redirect to 
 
 @app.route('/home')
 def homepage():
@@ -19,3 +39,8 @@ def homepage():
 #@app.route('/??profile??')
 #def profile_page():
 # return render_template('user_profile.html')
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
