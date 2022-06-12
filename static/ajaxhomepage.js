6abae48844f19c34d094ae4f36e3a216a02517c0
Megan <meganphaunef14@yahoo.com>
2022-06-12 17:13:14 -0400

    more cleaning

--- static/ajaxhomepage.js
+++ static/ajaxhomepage.js
@@ -16,16 +16,13 @@ get_recommendation_form.addEventListener('submit', (evt) => {
         .then((response) => response.json())
         .then((responseJson) => {
                 document.querySelector("#embed-iframe").insertAdjacentHTML('afterbegin', `<button class="favorite-button" value="${responseJson}">Favorite <i class="fa fa-heart"></i> </button> <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${responseJson}" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`)
-                console.log(responseJson)
                 go_button_clicked = true
         if (go_button_clicked === true) {
         
         const favorite_buttons = document.querySelectorAll('.favorite-button')
-        console.log(favorite_buttons)
         for (const favorite_button of favorite_buttons){
 
                 favorite_button.addEventListener('click', (evt) =>{
-                        console.log('i have been clicked')
                         const id = evt.target.value
                         if(confirm('Are you sure you want to add to favorites?'))
                         fetch(`/${id}/favorites`) 
